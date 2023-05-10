1fffc67a830f536bcd27e06251f48a2d559b7827
CupKido <taler@g.jct.ac.il>
2023-05-10 15:02:52 +0300

    added nav bar and css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Todos from './Todos'
 import Posts from './posts/Posts'
 import Albums from './Albums'
 import './App.css'
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import {BrowserRouter, Route, Routes, NavLink, redirect } from 'react-router-dom'
 import UserProvider from './UserContext'
 
 function App() {
@@ -11,7 +11,24 @@ function App() {
   return (
     <UserProvider>
       <BrowserRouter>
+        <nav>
+          <ul className="navbar">
+            <li>
+              <NavLink to="/login" activeClassName="active">Login</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Todos" activeClassName="active">Todos</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Posts" activeClassName="active">Posts</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Album" activeClassName="active">Album</NavLink>
+            </li>
+          </ul>
+        </nav>
         <Routes>
+        <Route path="/" element={<redirect to="/login" />} />
           <Route path="/login" element={<Login />} />
           <Route path="/Todos" element={<Todos />} />
           <Route path="/Posts" element={<Posts />} />
